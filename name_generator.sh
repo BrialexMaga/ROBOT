9fbf27820e8e4f8b93d7869cdd6911056bacb60a
Brian Magana <brian.magana2929@alumnos.udg.mx>
2023-07-17 14:31:50 -0600

    Upgrade name list and alias list

--- name_generator.sh
+++ name_generator.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-names=("Manolo" "Byte" "Megatron" "Techtron" "Optimus" "Circuit" "Nano" "PowerDrive" "Titan" "RoboMax" "Cell")
-alias=("cabeza de huevo metalico" "el robot" "Bot" "Mecanico" "de metal")
+names=("Manolo" "Byte" "Megatron" "Techtron" "Optimus" "Circuit" "Nano" "PowerDrive" "Titan" "RoboMax" "Cell" "TechWiz" "DataGenius" "InfoSeeker" "CodeWhiz" "BrainWave" "TaskMaster" "Helper" "SmartGenie" "DigitalSage" "Analytica" "Insight" "Prodigy" "SkillCraft")
+alias=("cabeza de huevo" "el robot" "Bot" "Guru" "Assistant" "Pro" "Buddy" "Companion" "Wizard" "Expert" "Sidekick" "Advisor")
 
 random_name=${names[$RANDOM % ${#names[@]} ]}
 random_alias=${alias[$RANDOM % ${#alias[@]} ]}
